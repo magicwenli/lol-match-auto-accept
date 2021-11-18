f98b49d30fea7d332e0806bdfb31c031321dd1e7
magicWenli <yxra3603@outlook.com>
2021-11-19 02:12:31 +0800

    add workflow

--- cmd/build.ps1
+++ cmd/build.ps1
@@ -3,4 +3,4 @@ $git_branch = (git branch --show-current)
 
 go install
 go build -o build\lmaa_$git_sha.exe main.go
-Write-Host "::set-output name=FileName::lmaa_$git_sha.exe"
+Write-Host "::set-output name=FileName::.\build\lmaa_$git_sha.exe"
